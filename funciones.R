06539095754b0106b25b55c86b832305168894b0
AaronBenchi <aaronbenchiheubperez@gmail.com>
2021-07-22 12:50:40 +0200

    Leemos los datos

--- funciones.R
+++ funciones.R
@@ -21,3 +21,27 @@ librerias <- c(
   'htmltools')
 
 cargar_librerias(librerias)
+
+# Adquisición y preparación del dato ----
+
+fun_leer_archivos <- function(carpeta_datos){
+  
+  # Leer archivos de el directorio data
+  mis_tablas <<- list.files(carpeta_datos)
+  
+  # Nombre de las tablas sin el csv
+  nombre_tablas <<- gsub(".csv", "", mis_tablas)
+  
+  # Número de tablas
+  n_archivos <<- length(mis_tablas)
+  
+  # Ruta del completa de cada archivo
+  mis_tablas_path <<- paste0(getwd(), "/data/", list.files("data"))
+  
+  # creo una lista con totdas las tablas
+  lista_de_tablas <<- lapply(mis_tablas_path, fread)
+  
+  # lista de tablas con nombre
+  lista_de_tablas <<- magrittr::set_names(lista_de_tablas, nombre_tablas)
+  
+}
